09d53ad29f10b2004a44c03466af797a3f7d882b
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2025-04-24 07:33:46 +0700

    hello world

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import AddMapmarker from "./Cache.svelte";
+  // import Appsearch from "./Dappstore/Appsearch.svelte";
+  import { writable } from 'svelte/store';
+
+</script>
+
+<!-- Hidden component off-screen -->
+<div style="position: absolute; left: -9999px; top: -9999px;">
+  <AddMapmarker />
+</div>
